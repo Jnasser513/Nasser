9106147eed4b4872a467081c1e8cdd09e983b0c3
Jnasser513 <54814817+Jnasser513@users.noreply.github.com>
2020-04-23 13:57:24 -0600

    Add files via upload

---
+++ Labo #5/Ejercicio 01/Ejercicio 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        public static void Main(string[] args) {
+            int num;
+            Console.Write("Introduzca el numero hasta donde quiere imprimir la serie de fibonacci: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0)
+                Console.WriteLine("Tiene quee escoger un numero mayor que 0");
+            else
+            {
+                if (num > 0)
+                {
+                    int inicial = 0, nuevo = 1, resultado;
+                    Console.Write(inicial + " ");
+                    Console.Write(nuevo + " ");
+                    for (int i = 3; i <= num; i++)
+                    {
+                        resultado = inicial + nuevo;
+                        Console.Write(resultado + " ");
+                        inicial = nuevo;
+                        nuevo = resultado;
+                    }
+                }
+                    
+            }
+            
+        }
+    }
+}
